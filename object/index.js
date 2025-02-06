01f125058172a9831550a7d6306c8a05830762b2
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2025-02-06 16:48:24 +0530

    add

---
+++ object/index.js
@@ -0,0 +1,49 @@
+let studant = {
+    name:"Dibya",
+    age:12,
+    about: ()=>{
+        console.log("I am ok");
+    },
+    address:{
+        state:"odisha",
+        village:"damodarnali"
+
+    }
+}
+
+let obj = new Object();
+
+let ans = Object.entries(studant);
+console.log(ans)
+let ans2 = Object.keys(studant);
+console.log(ans2)
+
+
+
+
+
+
+
+
+const empData = {
+    "dibya":45,
+    "sam":566,
+    "fin":366
+}
+
+class Util{
+    constructor(data){
+        this.data=data;
+    }
+    avaregesal(){
+        
+    }
+}
+
+let util = new Util(empData);
+
+
+
+
+
+
